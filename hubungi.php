2b76bc890b06f42752d3fe4a772196ec763a44e5
gilbertnadeak <49936751+gilbertnadeak@users.noreply.github.com>
2019-04-24 09:54:39 -0700

    Add files via upload

---
+++ hubungi.php
@@ -0,0 +1,5 @@
+<center>
+	<h2>Hubungi saya di</h2><br>
+		<i class="glyphicon glyphicon-envelope"></i> Gilbert.Hizkia@gmail.com<br>
+		<i class="glyphicon glyphicon-phone"></i> <a href="">085261926159</a>
+</center>
---
+++ indextext.php
@@ -0,0 +1,3 @@
+<center>
+	<h2>Selamat datang di Gilbert nadeak</h2>
+</center>
